c5e84f5f203f4ba57b0c6f7c717f40a720c2b25d
Mingyang Han <mingyang_han@esri.com>
2021-04-15 15:05:06 +0800

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, "src", "index.jsx"),
-    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "index.bundle.js"
+    },
     mode: process.env.NODE_ENV || "development",
-    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-    devServer: { contentBase: path.join(__dirname, "src") },
+    resolve: {
+        modules: [path.resolve(__dirname, "src"), "node_modules"],
+        extensions: [".js", ".json", ".jsx", ".css"]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "src")
+    },
     module: {
         rules: [
-            { 
-                test: /\.(js|jsx)$/, 
-                exclude: /node_modules/, 
-                use: ["babel-loader"] 
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
             },
             {
                 test: /\.(css|scss)$/,
                 use: ["style-loader", "css-loader"],
             },
-            { 
+            {
                 test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-                use: ["file-loader"] 
+                use: ["file-loader"]
             },
         ],
     },
@@ -29,4 +37,7 @@ module.exports = {
             template: path.join(__dirname, "src", "index.html"),
         }),
     ],
+    node: {
+        fs: "empty"
+    }
 };
