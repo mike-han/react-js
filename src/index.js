7d40aea0a64bc148373c658027f8d9024f3c644a
Mingyang Han <mingyang_han@esri.com>
2021-11-04 10:25:10 +0800

    Update demo

--- src/index.js
+++ src/index.js
@@ -1,75 +1,8 @@
 import { createChart } from './arcgis-charts/arcgis-charts-js'
-import barConfig from './bar-config'
-import pieConfig from './pie-config'
+import config1 from './configs/config1'
+import config2 from './configs/config2'
+import config3 from './configs/config3'
 
-const dataSource1 = {
-  type: 'inline',
-  data: [
-    {
-      FID_count: 28,
-      Year: '2,010',
-      objectid: 0
-    },
-    {
-      FID_count: 31,
-      Year: '2,011',
-      objectid: 1
-    },
-    {
-      FID_count: 27,
-      Year: '2,012',
-      objectid: 2
-    },
-    {
-      FID_count: 20,
-      Year: '2,013',
-      objectid: 3
-    },
-    {
-      FID_count: 19,
-      Year: '2,014',
-      objectid: 4
-    },
-    {
-      FID_count: 9,
-      Year: '2,015',
-      objectid: 5
-    }
-  ],
-  processed: true
-}
-
-const dataSource2 = {
-  type: 'inline',
-  data: [
-    {
-      "OBJECTID_sum": 892,
-      "ST": "AK",
-      "objectid": 0
-    },
-    {
-      "OBJECTID_sum": 35468,
-      "ST": "AL",
-      "objectid": 1
-    },
-    {
-      "OBJECTID_sum": 81344,
-      "ST": "AR",
-      "objectid": 2
-    },
-    {
-      "OBJECTID_sum": 62157,
-      "ST": "AZ",
-      "objectid": 3
-    },
-    {
-      "OBJECTID_sum": 519199,
-      "ST": "CA",
-      "objectid": 4
-    }
-  ],
-  processed: true,
-}
 
 const chartRoot = document.getElementById('root')
 
@@ -77,12 +10,11 @@ const renderButton = document.getElementById('render')
 renderButton.addEventListener('click', renderChart)
 
 const updateButton = document.getElementById('update')
-
 updateButton.addEventListener('click', updateChart)
 
 let chart = null
 function renderChart() {
-  let config = { ...pieConfig, dataSource: dataSource1 }
+  let config = config1
   console.log('createChart-start')
   createChart({
     chartConfig: config,
@@ -95,12 +27,24 @@ function renderChart() {
 }
 
 function updateChart() {
-  let config = { ...pieConfig, dataSource: dataSource2 }
+  let config = config2
   console.log('updateChart-start')
   chart.update({
     newChartConfig: config,
-    updateOptions: { dataUpdated: false }
+    updateOptions: { dataUpdated: true }
   }).then((res) => {
     console.log('updateChart-done')
+    updateChart2()
+  })
+}
+
+function updateChart2() {
+  let config = config3
+  console.log('updateChart2-start')
+  chart.update({
+    newChartConfig: config,
+    updateOptions: { dataUpdated: true }
+  }).then((res) => {
+    console.log('updateChart2-done')
   })
 }
