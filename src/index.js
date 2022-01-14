6dafba6db53e9d9bb8d64392cbe67d30efe3f915
Mingyang Han <mingyang_han@esri.com>
2022-01-14 11:26:15 +0800

    feat: make a chart stuck demo

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import { createChart } from './arcgis-charts/arcgis-charts-js'
-import config1 from './configs/config1'
-import config2 from './configs/config2'
-import config3 from './configs/config3'
+import { createChart as _createChart } from './arcgis-charts/arcgis-charts-js'
+import config from './config'
 
 
 const chartRoot = document.getElementById('root')
 
-const renderButton = document.getElementById('render')
-renderButton.addEventListener('click', renderChart)
+const createButton = document.getElementById('render')
+createButton.addEventListener('click', createChart)
 
 const updateButton = document.getElementById('update')
 updateButton.addEventListener('click', updateChart)
 
 let chart = null
-function renderChart() {
-  let config = config1
+function createChart() {
   console.log('createChart-start')
-  createChart({
+  _createChart({
     chartConfig: config,
     chartContainer: chartRoot,
     options: { dataUpdated: true }
@@ -27,24 +24,11 @@ function renderChart() {
 }
 
 function updateChart() {
-  let config = config2
   console.log('updateChart-start')
   chart.update({
     newChartConfig: config,
     updateOptions: { dataUpdated: true }
   }).then((res) => {
     console.log('updateChart-done')
-    updateChart2()
-  })
-}
-
-function updateChart2() {
-  let config = config3
-  console.log('updateChart2-start')
-  chart.update({
-    newChartConfig: config,
-    updateOptions: { dataUpdated: true }
-  }).then((res) => {
-    console.log('updateChart2-done')
   })
 }
