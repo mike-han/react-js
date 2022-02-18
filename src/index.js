e31d8a5a937066b3e6a738a307f183ebbb16f091
Mingyang Han <mingyang_han@esri.com>
2022-02-18 17:06:01 +0800

    feat: make a demo for arcgis-charts label overlap for diff versions

--- src/index.js
+++ src/index.js
@@ -1,50 +1,64 @@
-import { createChart } from './arcgis-charts/arcgis-charts-js'
+import { createChart as createChart1 } from './arcgis-charts/v1.0.0-beta.193/arcgis-charts-js'
+import { createChart as createChart2 } from './arcgis-charts/v1.0.0-beta.208/arcgis-charts-js'
 import config1 from './configs/config1'
 import config2 from './configs/config2'
-import config3 from './configs/config3'
+
+const root1 = document.getElementById('root1')
+const render1 = document.getElementById('render1')
+render1.addEventListener('click', () => {
+  renderChart1()
+  setTimeout(() => {
+    updateChart1()
+  }, 1000)
+})
 
 
-const chartRoot = document.getElementById('root')
-
-const renderButton = document.getElementById('render')
-renderButton.addEventListener('click', renderChart)
-
-const updateButton = document.getElementById('update')
-updateButton.addEventListener('click', updateChart)
-
-let chart = null
-function renderChart() {
+let chart1 = null
+function renderChart1() {
   let config = config1
-  console.log('createChart-start')
-  createChart({
+  createChart1({
     chartConfig: config,
-    chartContainer: chartRoot,
+    chartContainer: root1,
     options: { dataUpdated: true }
   }).then((res) => {
-    console.log('createChart-done')
-    chart = res
+    chart1 = res
   })
 }
 
-function updateChart() {
+function updateChart1() {
   let config = config2
-  console.log('updateChart-start')
-  chart.update({
+  chart1.update({
     newChartConfig: config,
     updateOptions: { dataUpdated: true }
-  }).then((res) => {
-    console.log('updateChart-done')
+  })
+}
+
+
+const root2 = document.getElementById('root2')
+const render2 = document.getElementById('render2')
+render2.addEventListener('click', () => {
+  renderChart2()
+  setTimeout(() => {
     updateChart2()
+  }, 1000)
+})
+
+let chart2 = null
+function renderChart2() {
+  let config = config1
+  createChart2({
+    chartConfig: config,
+    chartContainer: root2,
+    options: { dataUpdated: true }
+  }).then((res) => {
+    chart2 = res
   })
 }
 
 function updateChart2() {
-  let config = config3
-  console.log('updateChart2-start')
-  chart.update({
+  let config = config2
+  chart2.update({
     newChartConfig: config,
     updateOptions: { dataUpdated: true }
-  }).then((res) => {
-    console.log('updateChart2-done')
   })
 }
