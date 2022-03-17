fbd6409582898f26b5e65188f3397acaabd288b3
Mingyang Han <mingyang_han@esri.com>
2022-03-17 10:42:13 +0800

    fix: make a demo for chart stuck

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # The demo of arcgis-cahrts
 
-- Clone this repo and checkout to branch `arcgis-charts-v1.0.0-beta.169`
+- Clone this repo and checkout to branch `arcgis-charts-stuck-v1.0.0-beta.224`
 
-- Copy `arcgis-charts-js` into `src/arcgis-charts`
+- Copy `arcgis-charts-js-v1.0.0-beta.224` into `src/arcgis-charts`
 
 - Run `npm ci`
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import { createChart as _createChart } from './arcgis-charts/arcgis-charts-js'
 import config from './config'
 
-
 const chartRoot = document.getElementById('root')
+const create = document.getElementById('create')
 
-const createButton = document.getElementById('render')
-createButton.addEventListener('click', createChart)
-
-const updateButton = document.getElementById('update')
-updateButton.addEventListener('click', updateChart)
+create.addEventListener('click', () => {
+  createChart()
+})
 
 let chart = null
 function createChart() {
@@ -20,15 +18,5 @@ function createChart() {
   }).then((res) => {
     console.log('createChart-done')
     chart = res
-  })
-}
-
-function updateChart() {
-  console.log('updateChart-start')
-  chart.update({
-    newChartConfig: config,
-    updateOptions: { dataUpdated: true }
-  }).then((res) => {
-    console.log('updateChart-done')
   })
 }
