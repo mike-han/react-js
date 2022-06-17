d6aabb3c686577e9befcbb379266784fc7573bc9
mng <mng.han@foxmail.com>
2022-06-17 11:01:56 +0800

    fix: make a demo for chart selection

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # The demo of arcgis-cahrts
 
-- Clone this repo and checkout to branch `chart-js-v1.0.0-beta.280-pie-selection`
+- Clone this repo and checkout to branch `chart-js-v1.0.0-beta.280-selection`
 
 - Run `npm ci`
 
--- src/index.js
+++ src/index.js
@@ -1,46 +1,53 @@
-// import { createChart as _createChart } from './arcgis-charts/charts-js-250' //v1.0.0-beta.250
-import { createChart as _createChart } from '@arcgis/charts-js' //v1.0.0-beta.280
+// import { createChart as createChart } from './arcgis-charts/charts-js-250' //v1.0.0-beta.250
+import { createChart as createChart } from '@arcgis/charts-js' //v1.0.0-beta.280
 import config from './config'
 const inputData = config.dataSource.data
 
 const chartRoot = document.getElementById('root')
-const create = document.getElementById('create')
-const selection = document.getElementById('select')
-
-
-create.addEventListener('click', () => {
-  createChart()
-})
+const createBtn = document.getElementById('create')
+const selecAllBtn = document.getElementById('select-all')
+const selecNullBtn = document.getElementById('select-null')
+const selecByIndexBtn = document.getElementById('select-by-index')
+const clearSelection = document.getElementById('clear-selection')
+const consolePanel = document.getElementById('console')
 
 let chart = null
-function createChart() {
-  _createChart({
+
+createBtn.addEventListener('click', () => makeChart())
+selecNullBtn.addEventListener('click', () => updateChartSelectionItems([]))
+selecAllBtn.addEventListener('click', () => updateChartSelectionItems(inputData))
+clearSelection.addEventListener('click', () => chart && chart.clearSelection())
+
+let index = 0
+selecByIndexBtn.addEventListener('click', () => {
+  const selectionItems = [inputData[index]]
+  index ++
+  if(index >= inputData.length) {
+    index = 0
+  }
+  updateChartSelectionItems(selectionItems)
+})
+
+chartRoot.addEventListener('arcgisChartsJSSelectionComplete', (e) => {
+  const selectionSource = e.detail.selectionSource
+  consolePanel.textContent = selectionSource + ""
+})
+
+
+function makeChart() {
+  consolePanel.textContent = ""
+  createChart({
     chartConfig: config,
     chartContainer: chartRoot,
     options: { dataUpdated: true }
-  }).then((res) => {
-    chart = res
-  })
+  }).then((res) => chart = res)
 }
 
-let index = 0
-selection.addEventListener('click', () => {
+function updateChartSelectionItems(selectionItems){
   if(!chart) return
-  const selectionItems = getSelectionItems(index)
   const selectionData = { selectionItems }
   chart.update({
     newChartConfig: config,
     updateOptions: { selectionData }
   })
-  index ++
-  if(index >= inputData.length) {
-    index = 0
-  }
-})
-
-function getSelectionItems(index) {
-  const item = inputData[index]
-  const FID_count = item.FID_count
-  const Year = item.Year
-  return [{FID_count, Year}]
 }
