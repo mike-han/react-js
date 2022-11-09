59d7be5aa858080a6c349b5abcdc806574c2d081
mng <mng.han@foxmail.com>
2022-11-09 11:13:37 +0800

    fix9

--- src/index.jsx
+++ src/index.jsx
@@ -5,20 +5,36 @@ import { createChart } from '@arcgis/charts-js'
 import config from './config.json'
 
 const Root = () => {
-  const containerRef = React.useRef(null)
 
-  const handleClick = () => {
+  React.useEffect(() => {
+    const container1 = document.getElementById('container1')
     createChart({
       chartConfig: config,
-      chartContainer: containerRef.current,
+      chartContainer: container1,
       options: { dataUpdated: true }
     })
-  }
+    setTimeout(() => {
+      const container2 = document.getElementById('container2')
+      createChart({
+        chartConfig: config,
+        chartContainer: container2,
+        options: { dataUpdated: true }
+      })
+      const container3 = document.getElementById('container3')
+      createChart({
+        chartConfig: config,
+        chartContainer: container3,
+        options: { dataUpdated: true }
+      })
+    }, 1000)
+
+  }, [])
 
 
   return <div className='root'>
-    <div className="chart-container" ref={containerRef}></div>
-    <button className='cte-btn' onClick={handleClick}>Create chart</button>
+    <div className="chart-container" id="container1"></div>
+    <div className="chart-container" id="container2"></div>
+    <div className="chart-container" id="container3"></div>
   </div>
 }
 
