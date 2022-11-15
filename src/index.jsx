149d4a104bd6065963fc9cc060f9af2733807c5e
mng <mng.han@foxmail.com>
2022-11-15 11:20:16 +0800

    make a demo for v403

--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyPolyfills, defineCustomElements } from '@arcgis/charts-components/dist/loader'
 import './style.css';
-import { createChart } from '@arcgis/charts-js'
-import config from './config.json'
+import config1 from './config1/config1.json'
+import data1 from './config1/data1.json'
+import config2 from './config2/config2.json'
+import data2 from './config2/data2.json'
+import config3 from './config3/config3.json'
+import data3 from './config3/data3.json'
+import config4 from './config4/config4.json'
+import data4 from './config4/data4.json'
+
+
+applyPolyfills().then(() => {
+  defineCustomElements(window, { resourcesUrl: '../arcgis-charts/' })
+})
 
 const Root = () => {
+  const ref = React.useRef(null)
 
   React.useEffect(() => {
-    const container1 = document.getElementById('container1')
-    createChart({
-      chartConfig: config,
-      chartContainer: container1,
-      options: { dataUpdated: true }
-    })
-    setTimeout(() => {
-      const container2 = document.getElementById('container2')
-      createChart({
-        chartConfig: config,
-        chartContainer: container2,
-        options: { dataUpdated: true }
-      })
-      const container3 = document.getElementById('container3')
-      createChart({
-        chartConfig: config,
-        chartContainer: container3,
-        options: { dataUpdated: true }
-      })
-    }, 1000)
+    ref.current.config = config1
+    setTimeout(() => { ref.current.inlineData = { inputData: data1, processed: true } })
+  }, []) 
 
-  }, [])
+  const handleClick1 = () => {
+    ref.current.config = config2
+    setTimeout(() => { ref.current.inlineData = { inputData: data2, processed: true } })
+  }
+  const handleClick2 = () => {
+    ref.current.config = config3
+    setTimeout(() => { ref.current.inlineData = { inputData: data3, processed: true } })
+  }
+  const handleClick3 = () => {
+    ref.current.config = config4
+    setTimeout(() => { ref.current.inlineData = { inputData: data4, processed: true } })
+  }
 
 
   return <div className='root'>
-    <div className="chart-container" id="container1"></div>
-    <div className="chart-container" id="container2"></div>
-    <div className="chart-container" id="container3"></div>
+    <arcgis-charts-pie-chart ref={ref} />
+    <button onClick={handleClick1}>Configure colors based on grouping fields</button>
+    <button onClick={handleClick2}>Modified the color of a slice</button>
+    <button onClick={handleClick3}>Back to single color mode</button>
   </div>
 }
 
